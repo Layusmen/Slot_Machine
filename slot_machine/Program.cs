f2cc0aec88d49e7c85ff98a101ee6a77a739d2d5
Olayode Yusuff <getlayus@gmail.com>
2023-11-02 23:03:36 +0000

    Diggonal, vertical and horizontal win refactored

--- slot_machine/Program.cs
+++ slot_machine/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-
-
 namespace Slot_Machine
 {
     static class Program
@@ -12,19 +10,19 @@ namespace Slot_Machine
             const int COLUMN_COUNT = 3;
 
             // First messages
-            Console.WriteLine(@"Welcome to the Amazing Slot Machine!
-            Spin the reels and win big!
-            Match three symbols in any direction to win $
-            Get two lines of matching symbols to win $
-            Hit the Jackpot with three lines of matching symbols and win $
-            Ready to try your luck? A spin costs $");
+            Console.WriteLine("Welcome to the Amazing Slot Machine!");
+            Console.WriteLine("Spin the reels and win big!");
+            Console.WriteLine("Match three symbols in any direction to win $");
+            Console.WriteLine("Get two lines of matching symbols to win $");
+            Console.WriteLine("Hit the Jackpot with three lines of matching symbols and win $");
+            Console.WriteLine("Ready to try your luck? A spin costs $");
 
             // Random Generator
             Random randomPickGenerator = new Random();
-            List<char> slotSymbols = new List<char>()
-             {
-                    'A', '1', '5', '7', '$', 'M', '8', '9', '!', '#', 'Q', '&', 'C', 'S', 'Y', 'V', 'W', 'R', 'L', 'F'
-              };
+            List<char> slotSymbols = new List<char>
+            {
+                'A', '1', '5', '7', '$', 'M', '8', '9', '!', '#', 'Q', '&', 'C', 'S', 'Y', 'V', 'W', 'R', 'L', 'F'
+            };
 
             char[,] slots_Output = new char[ROW_COUNT, COLUMN_COUNT];
 
@@ -44,36 +42,81 @@ namespace Slot_Machine
 
             // Check for a win on horizontal lines
             bool horizontalWin = false;
-
-            for (int row = 0; row < ROW_COUNT; row++)
+            for (int row = 0; row < slots_Output.GetLength(0); row++)
             {
-                if (slots_Output[row, 0] == slots_Output[row, 1] && slots_Output[row, 1] == slots_Output[row, 2])
+                // Initialize a flag for the current row
+                bool isHorizontalWin = true;
+
+                for (int column = 1; column < slots_Output.GetLength(1); column++)
+                {
+                    // Compare the current symbol to the first symbol in the row
+                    if (slots_Output[row, column] != slots_Output[row, 0])
+                    {
+                        // If any symbol is different, set the flag to false and break
+                        isHorizontalWin = false;
+                    }
+                }
+
+                // If isHorizontalWin is still true, there is a win on this row
+                if (isHorizontalWin)
                 {
                     horizontalWin = true;
+                    // You can also break here if you want to stop checking the other rows
                 }
             }
+
 
             // Check for a win on vertical lines
             bool verticalWin = false;
             for (int column = 0; column < COLUMN_COUNT; column++)
             {
-                if (slots_Output[0, column] == slots_Output[1, column] && slots_Output[1, column] == slots_Output[2, column])
+                bool isVerticalWin = true;
+                for (int row = 1; row < ROW_COUNT; row++)
+                {
+                    if (slots_Output[row, column] != slots_Output[0, column])
+                    {
+                        isVerticalWin = false;
+
+                    }
+                }
+                if (isVerticalWin)
                 {
                     verticalWin = true;
                 }
             }
 
-            // Check for a win on the diagonal lines
+            // Check for a win on diagonal lines
             bool diagonalWin = false;
 
-            if (slots_Output[0, 0] == slots_Output[1, 1] && slots_Output[1, 1] == slots_Output[2, 2])
+            // Check the main diagonal (top-left to bottom-right)
+            bool isMainDiagonalWin = true;
+            for (int i = 1; i < ROW_COUNT; i++)
+            {
+                if (slots_Output[i, i] != slots_Output[0, 0])
+                {
+                    isMainDiagonalWin = false;
+                    break;
+                }
+            }
+            if (isMainDiagonalWin)
             {
                 diagonalWin = true;
             }
-            else if (slots_Output[0, 2] == slots_Output[1, 1] && slots_Output[1, 1] == slots_Output[2, 0])
+
+            // Check the secondary diagonal (top-right to bottom-left)
+            bool isSecondaryDiagonalWin = true;
+            for (int i = 1; i < ROW_COUNT; i++)
+            {
+                if (slots_Output[i, ROW_COUNT - 1 - i] != slots_Output[0, ROW_COUNT - 1])
+                {
+                    isSecondaryDiagonalWin = false;
+                }
+            }
+            if (isSecondaryDiagonalWin)
             {
                 diagonalWin = true;
             }
+
 
             // Check for a win and display the outcome
             if (horizontalWin || verticalWin || diagonalWin)
